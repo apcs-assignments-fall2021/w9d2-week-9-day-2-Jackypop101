f8677105079fbcd01f135521c70e8687cec87d39
Jack Chen <23hchen@athenian.org>
2021-10-28 10:35:16 -0700

    working

--- src/main/java/NinthGrader.java
+++ src/main/java/NinthGrader.java
@@ -1,3 +1,3 @@
-public class NinthGrader { // SOME CODE NEEDED IN THIS LINE
-    // YOUR CODE HERE
+public class NinthGrader extends Student { // SOME CODE NEEDED IN THIS LINE
+
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,29 @@
 public class Person {
+    private String first_name;
+    private String last_name;
+
+    public Person (String f, String l){
+        first_name = f;
+        last_name = l;
+    }
+
+    public void setfirstname (String x) {
+        this.first_name = x;
+    }
+    public void setlastname (String x) {
+        this.last_name = x;
+    }
+    public String getfirstname () {
+
+        return(this.first_name);
+    }
+    public String getlastname () {
+        return(this.last_name);
+    }
+    public String tostring (){
+        return("First name: " + this.first_name + "\nLast name: " + this.last_name);
+    }
+    public String dosomething(){
+        return("I am watching netflex.");
+    }
 }
